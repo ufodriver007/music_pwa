bcdec01b346d8a48b66c2682d64ba21115052f66
ufodriver007 <nemo_122@mail.ru>
2024-04-01 05:52:54 +0300

    fixed allowed hosts

--- music_site/settings.py
+++ music_site/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '89.23.110.30']
 
 
 # Application definition
