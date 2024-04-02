32d2a2d3d496498378abb34a02e62a9eed373d47
ufodriver007 <nemo_122@mail.ru>
2024-04-02 06:56:03 +0300

    fixed hosts settings

--- music_site/settings.py
+++ music_site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = DJANGO_SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '89.23.110.30']
+ALLOWED_HOSTS = ['127.0.0.1', '89.23.110.30', 'www.mint-coast.ru']
 
 
 # Application definition
