dfa28b8c04b40aef5cc0e0e7f38b1bfe78a1d74f
ufodriver007 <nemo_122@mail.ru>
2024-04-27 18:16:35 +0300

    test vkID auth46

--- static/js/main.js
+++ static/js/main.js
@@ -176,7 +176,7 @@ const container = document.getElementById('VkIdSdkOneTap');
 // Проверка наличия кнопки в разметке.
 if (container) {
   // Отрисовка кнопки в контейнере с именем приложения APP_NAME, светлой темой и на русском языке.
-  oneTap.render({ container: container, scheme: VKID.Scheme.LIGHT, lang: VKID.Languages.RUS, showAlternativeLogin: true });
+  oneTap.render({ container: container, scheme: VKID.Scheme.DARK, lang: VKID.Languages.RUS, showAlternativeLogin: true });
 }
 
 //login
